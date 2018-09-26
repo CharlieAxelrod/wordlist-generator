cc6600328f6f61b26a020c6dcf95ea6c0e66ca95
Hack Reactor Student <student@hackreactor.com>
2018-09-25 18:12:34 -0700

    Refactored using check-word library.

--- wordlist.js
+++ wordlist.js
@@ -1,14 +1,16 @@
 var genWordlist = function(n, letters) {
   var results = [];
-  var words = require('an-array-of-english-words');
-  var subWords = words.filter(word => word.includes('legal'));
-  console.log(subWords);
+  var checkWord = require('check-word');
+  var words = checkWord('en');
   
   var helper = function(n, cache) {
     for (var i = 0; i < letters.length; i++) {
       cache += letters[i];
       if (cache.length === n) {
-        if (subWords.includes(cache)) results.push(cache);
+        if (words.check(cache)) {
+          console.log(cache); 
+          results.push(cache);
+        }
       } else {
         helper(n, cache);
       }
@@ -16,8 +18,5 @@ var genWordlist = function(n, letters) {
     }
   }
   helper(n, []);
-  console.log(results);
   return results;
-}
-
-genWordlist(9, ['l', 'e', 'g', 'a', 'p', 'r']);
+};
