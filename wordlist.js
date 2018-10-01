b089941283c28bfabaf18cf6b4140ffe8b4c773c
Charlie Axelrod <charlieaxelrod@gmail.com>
2018-09-30 21:51:13 -0700

    Refactored for simplicity.

--- wordlist.js
+++ wordlist.js
@@ -3,20 +3,20 @@ var genWordlist = function(n, letters) {
   var checkWord = require('check-word');
   var words = checkWord('en');
   
-  var helper = function(n, cache) {
+  var helper = function(cache) {
     for (var i = 0; i < letters.length; i++) {
       cache += letters[i];
       if (cache.length === n) {
-        if (words.check(cache)) {
-          console.log(cache); 
+        if (words.check(cache)) { 
           results.push(cache);
         }
       } else {
-        helper(n, cache);
+        helper(cache);
       }
       cache = cache.slice(0, -1);
     }
   }
-  helper(n, []);
+  helper("");
   return results;
 };
+
